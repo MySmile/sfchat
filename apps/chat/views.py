1cb8f3a2f31615d44c917cbd141a804ff766628e
Sergii <picamator@gmail.com>
2014-11-22 23:08:30 +0200

    Added core, renders and updated messages source in js. Updated home template, added new design elements on chat page.

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -4,15 +4,13 @@ from django.template import RequestContext
 from apps.chat.models import Chats
 from apps.home.views import e404
 from apps.chat.utils import ChatPage
+from apps.chat.forms import ChatTypeForm
+
 
 def chat(request, chat_token):
     if Chats.validate_chat_token(chat_token):
         chat_page = ChatPage(request)
-        c = {'user_token': chat_page.get_user_token()}
+        c = {'user_token': chat_page.get_user_token(), 'form': ChatTypeForm()}
         return render_to_response('chat.html', c, context_instance=RequestContext(request))
     else:
         return e404(request, template_name='404.html')
-
-
-def close(request):
-    pass
