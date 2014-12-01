859ed8b37733837afa0691b93d9baf1deb4db411
Sergii <picamator@gmail.com>
2014-12-01 21:16:07 +0200

    Added templatetag to protect email, added privacy politics, updated html in license

---
+++ apps/home/templatetags/email_protection.py
@@ -0,0 +1,24 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag
+def email_protection(email, hide_class):
+    """
+    Tag to protect email by adding span tag after each character, 
+    replace dot with html code, replace @ to [at], 
+    :param email: String
+    :param hise_class: String
+    :return: Mix
+    """
+    dot = '&#46;'
+    pattern = '<span class="' + hide_class + '">&nbsp</span>'
+    at = '[at]'
+
+    email = email.replace('@', at)
+    result = ''
+    for item in email:
+        result += item + pattern
+    return result.replace('.', dot)
