13b146d31354e4e618983360d0acf92f8d56d86c
Andrii Pryz <well.i.beta@gmail.com>
2014-12-17 17:42:16 +0200

    some tests added

--- apps/home/tests/test_view.py
+++ apps/home/tests/test_view.py
@@ -1,6 +1,7 @@
 import unittest
 
 from django.test.client import Client
+from django.http import HttpResponsePermanentRedirect
 
 from apps.home.forms import JoinChatForm
 from apps.chat.models import Chats
@@ -8,15 +9,20 @@ from apps.chat.models import Chats
 
 class HomePageTest(unittest.TestCase):
     def setUp(self):
-        self._client = Client()
+        self.client = Client()
         self.chat_token = Chats.create_chat()['chat_token']
 
     def test_index(self):
-        response = self._client.get('/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<div id="chat-entry">', response.content)
         self.assertIn(b'button id="create_chat"', response.content)
         self.assertIn(b'button id="join_chat"', response.content)
+
+    def test_redirect_after_create(self):
+        response = self.client.post('/create-chat/')
+        self.assertEqual(response.status_code, 301)
+        self.assertTrue(isinstance(response, HttpResponsePermanentRedirect))
 
     def test_form_JoinChat_success(self):
         form_data = {'chat_token': self.chat_token}
