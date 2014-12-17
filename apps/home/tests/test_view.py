cf3608d3a9d77c2eee673162c12987e4bba21e47
Andrii Pryz <well.i.beta@gmail.com>
2014-12-17 15:47:36 +0200

    some tests added

--- apps/home/tests/test_view.py
+++ apps/home/tests/test_view.py
@@ -1,11 +1,15 @@
 import unittest
 
 from django.test.client import Client
-from apps.home.views import HomeView
+
+from apps.home.forms import JoinChatForm
+from apps.chat.models import Chats
 
 
 class HomePageTest(unittest.TestCase):
-    _client = Client()
+    def setUp(self):
+        self._client = Client()
+        self.chat_token = Chats.create_chat()['chat_token']
 
     def test_index(self):
         response = self._client.get('/')
@@ -14,5 +18,12 @@ class HomePageTest(unittest.TestCase):
         self.assertIn(b'button id="create_chat"', response.content)
         self.assertIn(b'button id="join_chat"', response.content)
 
-    def test_form(self):
+    def test_form_JoinChat_success(self):
+        form_data = {'chat_token': self.chat_token}
+        form = JoinChatForm(data=form_data)
+        self.assertEqual(form.is_valid(), True)
 
+    def test_form_JoinChat_failed(self):
+        form_data = {'chat_token': '0'*24}
+        form = JoinChatForm(data=form_data)
+        self.assertEqual(form.is_valid(), False)
