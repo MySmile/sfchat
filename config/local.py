7b42cb8ae19db2bf2bb9df1f80102ff557860880
Andrii Pryz <well.i.beta@gmail.com>
2015-05-24 13:45:49 +0300

    update settings

--- config/local.py
+++ config/local.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # secret settings
 import os
-from mongoengine import connect, register_connection
+from mongoengine import connect
 
 from sfchat.settings.base import BASE_DIR
 
@@ -12,11 +12,6 @@ COMPRESS_ENABLED = False
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
 # @NOTE: if True then enable 'debug_toolbar.middleware.DebugToolbarMiddleware' also
-
-
-TEMPLATE_DEBUG = DEBUG
-
-INTERNAL_IPS = '127.0.0.1'
 
 SECRET_KEY = '&ku!ebrl5h61ztet=c&ydh+sc9tkq=b70^xbx461)l1pp!lgt6'
 
@@ -33,25 +28,14 @@ connect(MONGODB_DATABASE_NAME,
         password=MONGODB_PASSWORD)
 
 # Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
      'sfchat': {
          'ENGINE': 'django.db.backends.dummy',
      },
-      # 'sfchat': {
-        # 'ENGINE': 'django.db.backends.dummy',
-    #     #~ 'USER': '',
-    #     #~ 'PASSWORD': '',
-    #     #~ 'HOST': '',
-    #     #~ 'PORT': '',
-    #   },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'sfchat/db/sfchat_admin.sqlite3'),
-        # 'USER': '',
-        # 'PASSWORD': '',
-        # 'HOST': '',
-        # 'PORT': '',
     }
 }
 
@@ -68,6 +52,5 @@ GOOGLE_ANALYTICS_DEBUG_MODE = True
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'sfchat/static/')
 COMPRESS_ROOT = STATIC_ROOT
-
 
 DATABASE_ROUTERS = ['apps.chat.router.SFChatRouter', 'apps.chat.router.AdminRouter',]
--- sfchat/settings/production.py
+++ sfchat/settings/production.py
@@ -13,28 +13,38 @@ APP_MIDDLEWARE_CLASSES = (
     'apps.chat.middlewares.ExceptionLoggingMiddleware',
 )
 
-THIRD_PARTY_MIDDLEWARE_CLASSES = ()
+THIRD_PARTY_MIDDLEWARE_CLASSES = (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
 
 MIDDLEWARE_CLASSES = DJANGO_MIDDLEWARE_CLASSES + APP_MIDDLEWARE_CLASSES + THIRD_PARTY_MIDDLEWARE_CLASSES
 
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
+    'apps.adminpanel',
     'apps.api.v1',
     'apps.chat',
-    'apps.csp', #  content security policy
+    'apps.csp', # content security policy
     'apps.flatpages',
     'apps.home',
     'apps.sitemap',
 )
 
 
+# @TODO: enable
+#    'mongoengine.django.mongo_auth',
+#    'rest_framework.authtoken',
+# in THIRD_PARTY_APPS settings
+# and exclude ('mongo_auth', 'authtoken') in admin datadase with router
+
 # Third party apps
 THIRD_PARTY_APPS = (
+    # 'mongoengine.django.mongo_auth',
     'rest_framework',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
     'compressor',
-    'django_rq',
+    'debug_toolbar',
 )
 
 
@@ -57,3 +67,4 @@ CSRF_FAILURE_VIEW = 'apps.home.utils.csrf_failure'
 
 
 COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',  'compressor.filters.cssmin.CSSMinFilter']
+
