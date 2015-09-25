8b3f1b247fd56139555c8d16840956a2319702d8
Andrii Pryz <well.i.beta@gmail.com>
2015-09-25 18:58:47 +0200

    add robots.txt

--- apps/sitemap/urls.py
+++ apps/sitemap/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import patterns, url
+from django.http import HttpResponse
+from django.conf import settings
 
 
 urlpatterns = patterns('',
     url(r'^sitemap\.xml$', 'apps.sitemap.views.SitemapXML'),
+
+    #(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nHost: " + \
+                                              settings.ALLOWED_HOSTS[0] +
+                                              "\nSitemap: http://" + settings.ALLOWED_HOSTS[0] + \
+                                              "/sitemap.xml", content_type="text/plain")),
 )
